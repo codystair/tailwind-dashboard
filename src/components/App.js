9cb232faf982b401524aa71357415927c17dedd6
Cody Stair <ccstair27@gmail.com>
2020-11-19 14:52:06 -0500

    change

--- src/components/App.js
+++ src/components/App.js
@@ -1,80 +1,91 @@
-import React, { Component } from 'react';
-import '../scss/app.scss';
+import React, { Component } from "react";
+import "../scss/app.scss";
 
-import { Route } from "react-router-dom"
-import TopBar from './TopBar';
-import Sidebar from './Sidebar';
-import Body from './Body';
-import Dashboard from './pages/Dashboard';
-import Notifications from './pages/Notifications';
-import UserProfile from './pages/UserProfile';
-import Navigation from './pages/Navigation';
-import Input from './pages/Input';
+import { Route } from "react-router-dom";
+import TopBar from "./TopBar";
+import Sidebar from "./Sidebar";
+import Body from "./Body";
+import Dashboard from "./pages/Dashboard";
+import Notifications from "./pages/Notifications";
+import UserProfile from "./pages/UserProfile";
+import Navigation from "./pages/Navigation";
+import Input from "./pages/Input";
 
 const pages = [
   {
-      name: "Dashboard",
-      iconClass: "fas fa-home",
-      path: "/",
-      component: Dashboard
+    name: "Dashboard",
+    iconClass: "fas fa-home",
+    path: "/",
+    component: Dashboard,
   },
   {
-      name: "User Profile",
-      iconClass: "fas fa-user",
-      path: "/user-profile",
-      component: UserProfile
+    name: "User Profile",
+    iconClass: "fas fa-user",
+    path: "/user-profile",
+    component: UserProfile,
   },
   {
-      name: "Notifications",
-      iconClass: "fas fa-bell",
-      path: "/notifications",
-      component: Notifications
+    name: "Notifications",
+    iconClass: "fas fa-bell",
+    path: "/notifications",
+    component: Notifications,
   },
   {
-      name: "Navigation",
-      iconClass: "fas fa-bars",
-      path: "/navigation",
-      component: Navigation
+    name: "Navigation",
+    iconClass: "fas fa-bars",
+    path: "/navigation",
+    component: Navigation,
   },
   {
-      name: "Input",
-      iconClass: "fas fa-keyboard",
-      path: "/input",
-      component: Input
-  }
-]
+    name: "Input",
+    iconClass: "fas fa-keyboard",
+    path: "/input",
+    component: Input,
+  },
+];
 
 class App extends Component {
-
-  constructor(props){
+  constructor(props) {
     super(props);
     this.state = {
-      sidebarIsShown: true
-    }
-    this.toggleSidebar = ()=>{
-      this.setState({sidebarIsShown: !this.state.sidebarIsShown})
-    }
+      sidebarIsShown: true,
+    };
+    this.toggleSidebar = () => {
+      this.setState({ sidebarIsShown: !this.state.sidebarIsShown });
+    };
   }
-
 
   render() {
     return (
-      <Route render={({location})=>{
-
-        return <div className="app">
-          <TopBar toggleSidebar={this.toggleSidebar}/>
-          <div className="flex">
-            <Sidebar sidebarIsShown={this.state.sidebarIsShown} pages={pages} location={location}/>
-            <Body>
-              <h2>Test: {process.env.PATH_PREFIX}<h2>
-              {pages.map((page, index)=>{
-                return <Route exact path={page.path} key={index} component={page.component} />
-              })}
-            </Body>
-          </div>
-        </div>
-
-      }} />
+      <Route
+        render={({ location }) => {
+          return (
+            <div className="app">
+              <h2>testing...</h2>
+              <TopBar toggleSidebar={this.toggleSidebar} />
+              <div className="flex">
+                <Sidebar
+                  sidebarIsShown={this.state.sidebarIsShown}
+                  pages={pages}
+                  location={location}
+                />
+                <Body>
+                  {pages.map((page, index) => {
+                    return (
+                      <Route
+                        exact
+                        path={page.path}
+                        key={index}
+                        component={page.component}
+                      />
+                    );
+                  })}
+                </Body>
+              </div>
+            </div>
+          );
+        }}
+      />
     );
   }
 }
