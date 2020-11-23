d4ad7625174f3809822731cc7a820a66975ab511
Cody Stair <ccstair27@gmail.com>
2020-11-23 13:16:27 -0500

    remove basename

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import { BrowserRouter } from "react-router-dom";
 import { unregister } from "./registerServiceWorker";
 
 ReactDOM.render(
-  <BrowserRouter basename={process.env.PUBLIC_URL}>
+  <BrowserRouter>
     <App />
   </BrowserRouter>,
   document.getElementById("root")
