52af00df58da050c284debcf71ea6fb1e316c1aa
Cody Stair <ccstair27@gmail.com>
2020-11-30 15:00:48 -0500

    update sw

--- .github/stagehand/stagehand_sw.js
+++ .github/stagehand/stagehand_sw.js
@@ -1,8 +1,9 @@
-self.addEventListener("install", event => {
+self.addEventListener("install", (event) => {
+  self.skipWaiting();
   console.log("service worker installed");
 });
 
-self.addEventListener("activate", event => {
+self.addEventListener("activate", (event) => {
   clients.claim();
 });
 
@@ -12,11 +13,11 @@ caches.keys().then(function (names) {
 
 let basePath;
 
-self.addEventListener('message', (event) => {
+self.addEventListener("message", (event) => {
   if (event.data.resetBasepath) basePath = null;
 });
 
-const handleRequest = async event => {
+const handleRequest = async (event) => {
   console.log(event);
 
   let requestedUrl;
@@ -40,29 +41,30 @@ const handleRequest = async event => {
   const urlPathMatch = url.match(/[^\/]+\/\/[^\.]+\.cloudfront.net\/(.*)/);
 
   // => "branch/commit"
-  basePath = basePath || (referrerBasePathMatch && referrerBasePathMatch[1]) || '';
+  basePath =
+    basePath || (referrerBasePathMatch && referrerBasePathMatch[1]) || "";
 
-  console.log('basePath:', basePath);
+  console.log("basePath:", basePath);
   // check if the domains are both for same cloudfront
   // If they are we need to make sure it has basePath prepended
-  if ((referrerDomain && urlDomain) && (referrerDomain[0] === urlDomain[0])) {
-    let urlPath = urlPathMatch && urlPathMatch[1] || '';
+  if (referrerDomain && urlDomain && referrerDomain[0] === urlDomain[0]) {
+    let urlPath = (urlPathMatch && urlPathMatch[1]) || "";
     let req;
 
-    if (urlPath.startsWith('/')) urlPath = urlPath.slice(1);
-    if (urlPath.endsWith('/') || urlPath === '') urlPath += 'index';
-    if (urlPath && !urlPath.includes('.')) urlPath += '.html';
+    if (urlPath.startsWith("/")) urlPath = urlPath.slice(1);
+    if (urlPath.endsWith("/") || urlPath === "") urlPath += "index";
+    if (urlPath && !urlPath.includes(".")) urlPath += ".html";
 
     // if it already has basepath send req as is
-    if (urlPath.match(basePath)) {      
-      urlPath = urlDomain + '/' + urlPath
+    if (urlPath.match(basePath)) {
+      urlPath = urlDomain + "/" + urlPath;
 
-    // prepend basepath
+      // prepend basepath
     } else {
-      urlPath = urlDomain + '/' + basePath + '/' + urlPath;
+      urlPath = urlDomain + "/" + basePath + "/" + urlPath;
     }
 
-    console.log('urlPath:', urlPath);
+    console.log("urlPath:", urlPath);
 
     req = new Request(urlPath, {
       ...event.request,
@@ -71,16 +73,16 @@ const handleRequest = async event => {
     console.log("request:", req);
 
     return fetch(req)
-      .then(res => res)
-      .catch(err => console.log("err:", err));
+      .then((res) => res)
+      .catch((err) => console.log("err:", err));
   }
 
   // if not origin domain then just forward the initial request
   return fetch(event.request)
-    .then(res => res)
-    .catch(err => console.log("err:", err));
+    .then((res) => res)
+    .catch((err) => console.log("err:", err));
 };
 
-self.addEventListener("fetch", event => {
+self.addEventListener("fetch", (event) => {
   event.respondWith(handleRequest(event));
 });
