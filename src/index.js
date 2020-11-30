f961d69e900cec33b116ec587ed30bcb9c1ba858
Cody Stair <ccstair27@gmail.com>
2020-11-30 12:54:42 -0500

    remove unregister

--- src/index.js
+++ src/index.js
@@ -1,9 +1,14 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './tailwind.css';
-import App from './components/App';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./tailwind.css";
+import App from "./components/App";
 import { BrowserRouter } from "react-router-dom";
-import { unregister } from './registerServiceWorker';
+import { unregister } from "./registerServiceWorker";
 
-ReactDOM.render(<BrowserRouter><App /></BrowserRouter>, document.getElementById('root'));
-unregister();
+ReactDOM.render(
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>,
+  document.getElementById("root")
+);
+// unregister();
